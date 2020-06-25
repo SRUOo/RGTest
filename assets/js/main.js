17553556e28512033355bed3eeb907abbe2dc6ef
qcminecraft <qcmc@qcminecraft.com>
2020-06-25 22:50:57 +0800

    更新布局

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,46 @@
-(function(){
 "use strict";
-const ver = 0.2; 
+
+NProgress.start();
+
+$.ajax(
+    {
+        url:"data/data.json",
+        success:function(result){
+            NProgress.done();
+            window.rgData = result;
+            console.log(result);
+        }
+    }
+    
+);
 
 //console
 window.onload = function(){
+    hashChange();
     var now = new Date().getTime();
     var page_load_time = now-performance.timing.navigationStart;
     console.clear();
     console.log('%c今天你入关了吗？','font-size:2em');
     console.log('%c页面加载完毕，消耗'+Math.round(performance.now()*100)/100+'ms','background:#fff;color:#333;text-shadow:0 0 2px #eee,0 0 3px #eee,0 0 3px #eee,0 0 2px #eee,0 0 3px #eee;');
 };
-})
+
+
+//监听触发操作
+function hashChange(){
+    let q={};
+    location.hash.replace(/([^#&=]+)=([^&]+)/g,(_,k,v)=>q[k]=v);
+    window.RGQuery = q;
+    console.log(window.RGQuery);
+}
+
+//url变化监听器
+if (('onhashchange' in window) && ((typeof document.documentMode === 'undefined') || document.documentMode == 8)) {
+    window.onhashchange = hashChange;
+} else {
+    setInterval(function () {
+        let ischanged = isHashChanged();
+        if (ischanged) {
+            hashChange();
+        }
+    }, 150);
+}
